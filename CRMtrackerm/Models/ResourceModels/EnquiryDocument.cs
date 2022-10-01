bf8bb6881d9b34073fe61c19a552dba3583fbc5c
yericharla manu <y.manubabu@gmail.com>
2022-10-01 23:45:49 +0530

    commit

--- CRMtrackerm/Models/ResourceModels/EnquiryDocument.cs
+++ CRMtrackerm/Models/ResourceModels/EnquiryDocument.cs
@@ -5,7 +5,7 @@ namespace CrmTracker.Models.ResourceModels
 
 
     public class EnquiryDocument
-    {
+    { public int enquiry_id { get; set; }
         public int document_index { get; set; }
         public string document_path { get; set; }
         public string document_desc { get; set; }
